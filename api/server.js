0cdaa2fa0773e85d5cadb8203bd8ce6e9f4f91a5
Arison13 <arisonarias@gmail.com>
2021-11-30 00:30:20 -0500

    he client can make the get request to get users

--- api/server.js
+++ api/server.js
@@ -1,3 +1,22 @@
 // BUILD YOUR SERVER HERE
+const express = require ("express")
+const Users = require("./users/model")
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+
+const server = express()
+server.use(express.json())
+
+server.get('/api/users', (req, res)=> {
+    Users.find()
+    .then(user => {
+        res.json(user)
+    }).catch(err => {
+        res.status(500).json({
+             message: "The users information could not be retrieved",
+            error: err.message
+        })
+    })
+})
+
+
+module.exports = server 
--- api/users/model.js
+++ api/users/model.js
@@ -16,8 +16,6 @@ const initializeUsers = () => ([
 let users = initializeUsers()
 
 // DATABASE ACCESS FUNCTIONS
-// DATABASE ACCESS FUNCTIONS
-// DATABASE ACCESS FUNCTIONS
 const find = () => {
   // SELECT * FROM users;
   return Promise.resolve(users)
