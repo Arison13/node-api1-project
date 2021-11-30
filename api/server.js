0df4c121667a4c80a6b038de09084c8efa479b69
Arison13 <arisonarias@gmail.com>
2021-11-30 11:19:24 -0500

    can GET user by id endpoint

--- api/server.js
+++ api/server.js
@@ -6,11 +6,14 @@ const Users = require("./users/model")
 const server = express()
 server.use(express.json())
 
+
+//[GET]s all users
 server.get('/api/users', (req, res)=> {
     Users.find()
-    .then(user => {
-        res.json(user)
-    }).catch(err => {
+    .then(users => {
+        res.json(users)
+    })
+    .catch(err => {
         res.status(500).json({
              message: "The users information could not be retrieved",
             error: err.message
@@ -18,5 +21,24 @@ server.get('/api/users', (req, res)=> {
     })
 })
 
+//[GET]s users by id 
+server.get('/api/users/:id', (req, res)=> {
+    Users.findById(req.params.id)
+    .then(user => {
+        if(!user){
+            res.status(404).json({
+                message: "The user with the specified ID does not exist"
+            })
+        }
+        res.json(user)
+
+    })
+    .catch(err => {
+        res.status(500).json({
+             message: "The user information could not be retrieved",
+            error: err.message
+        })
+    })
+})
 
 module.exports = server 
